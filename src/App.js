a283116f64f02361cb97066c395e089b089102a4
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-14 13:32:11 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './componentes/Header';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/componentes/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <img src="/img/logo.png" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
