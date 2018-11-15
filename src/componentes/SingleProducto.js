9e17a4fd7e76b726e4a00f442f4061cb3dc7c112
Oswaldo <oswaldo.vazquez@gmail.com>
2018-11-15 14:11:13 -0600

    single product page working

--- src/componentes/SingleProducto.js
+++ src/componentes/SingleProducto.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import '../css/SingleProductos.css';
+//import '../css/Productos.css';
 
-const SingleProducto = () => {
+const SingleProducto = (props) => {
+   
+    const { imagen, nombre, precio, descripcion} = props.producto;
+    
     return (
-        <p>hola</p>
+        <div className="info-producto">
+            <div className="imagen">
+                <img src={`/img/${imagen}.png`} alt={nombre} />
+            </div>
+            <div className="info">
+                <h2>{nombre}</h2>
+                <p className="precio">$ {precio}</p>
+                <p>{descripcion}</p>
+            </div>
+        </div>
+        
     )
 }
 
