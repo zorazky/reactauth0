2ced1b210638d6a604df74c56362a646973c6525
Oswaldo <oswaldo.vazquez@gmail.com>
2019-01-08 12:28:55 -0600

    installing auth0

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Router from './componentes/Router';
+import { makeMainRoutes } from './componentes/routes';
+
+const routes = makeMainRoutes();
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Router />
+        {routes}
       </React.Fragment>
     );
   }
--- cliente/src/componentes/routes.js
+++ cliente/src/componentes/routes.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { Route, Router } from 'react-router-dom';
-import App from './App';
-import Home from './Home/Home';
+
+//Auth0
 import Callback from './Callback/Callback';
-import Auth from './Auth/Auth';
-import history from './history';
+import Auth from '../Auth/Auth';
+import history from '../history';
+
+//componentes propios
+import Header from './Header';
+import Navegacion from './Navegacion';
+import Productos from './Productos';
+import Nosotros from './Nosotros';
+import Contacto from './Contacto';
+import Error from './Error';
+import SingleProducto from './SingleProducto'
 
 const auth = new Auth();
 
@@ -17,9 +26,44 @@ const handleAuthentication = ({location}) => {
 export const makeMainRoutes = () => {
   return (
       <Router history={history}>
-        <div>
-          <Route path="/" render={(props) => <App auth={auth} {...props} />} />
-          <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+        <div className="contenedor">
+        <Header />
+        <Navegacion />
+
+          
+          <Route exact path="/" render={(props) => (
+
+              <Productos 
+                  //productos={resultado}
+                  busquedaProducto={this.busquedaProducto}
+                  auth={auth} {...props}
+              />
+
+          )} />
+          
+          <Route exact path="/nosotros" component={Nosotros} /> 
+          <Route exact path="/contacto" component={Contacto} />
+
+          <Route exact path="/productos" render={(props) => (
+            <Productos 
+                   // productos={resultado}
+                    busquedaProducto={this.busquedaProducto}
+                    auth={auth} {...props}
+                />
+            )} /> 
+
+            <Route exact path="/producto/:productoId" render={(props) => {
+              let idProducto = props.location.pathname.replace('/producto/','');
+              return (
+                  <SingleProducto 
+                      producto={this.state.productos[idProducto]}
+                      key={idProducto}
+                      auth={auth} {...props}
+                  />
+              )
+          }} />
+
+          
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
             return <Callback {...props} /> 
